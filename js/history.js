52a5ceadf813879424f2cffb90c25bae258ee2ad
jorgezerpa <jorgezerpacoder@gmail.com>
2022-02-27 09:04:01 -0400

    next step:fix small details and improve responsivness

--- js/history.js
+++ js/history.js
@@ -1,4 +1,6 @@
 const history__container = document.getElementById("history__container");
+let elements;
+let observer;
 
 class HistoryComponent{
     constructor( img, text, key){
@@ -20,27 +22,31 @@ const history_components = [
 ]
 
 
-let block_a = ( component)=>(
+let block_a = (component)=>(
     `
-    <div class="history__section">
-    <div class="history__line"><div class="pointer left"></div></div>
-    <div class="history__element">
-    <img class ="history__image" src="${component.img}">
-    </div>
-    <div class="history__element">
-        <div class="history__text"><p>${ component.text}</p></div>
-    </div>
+    <div class="history__section ">
+        <div class="history__line"><div class="pointer left"></div></div>
+        
+        <div class="history__element">
+            <img class ="history__image" src="${component.img}">
+        </div>
+
+        <div class="history__element">
+            <div class="history__text"><p>${ component.text}</p></div>
+        </div>
     </div>
 `          
 )
 
-let block_b = ( component)=>(
+let block_b = (component)=>(
     `
     <div class="history__section">
+        <div class="history__line"><div class="pointer right"></div></div>
+
         <div class="history__element">
             <div class="history__text"><p>${component.text}</p></div>
         </div>
-        <div class="history__line"><div class="pointer right"></div></div>
+        
         <div class="history__element">
             <img class ="history__image" src="${component.img}">
         </div>
@@ -49,9 +55,52 @@ let block_b = ( component)=>(
 )
 
 history_components.forEach((component, i)=>{
-    let is_par = i%2 === 0;
-    history__container.innerHTML += is_par ? block_a(component) : block_b(component)
+    let is_par = i%2 === 0; //change beetween both components
+    history__container.innerHTML += is_par ? block_a(component) : block_b(component);
+ 
+    if(i===history_components.length-1) elements = document.querySelectorAll(".history__element"); 
 })
 
+ 
+ const displayAnimations = (entries, observer)=>{
+        entries.forEach((entrie, i)=>{
+            if(entrie.isIntersecting){
+                if(i % 2 === 0) entrie.target.classList.add("history__element_isIntersecting1");
+                else entrie.target.classList.add("history__element_isIntersecting2");
+            }
+        })    
+    }
+
+ observer = new IntersectionObserver(displayAnimations, {
+        root: null,
+        rootMargin: '0px 0px 0px 0px',
+        threshold: 0.5,     
+ })
+
+elements.forEach(element=>observer.observe(element))
+
+
+// const elements = document.querySelectorAll(".section");
+
+// const showImage = (entries, observer)=>{
+//     // console.log(entries);
+//     // console.log(observer);
+//     entries.forEach(entrie=>{
+//         if(entrie.isIntersecting){
+//             entrie.target.classList.add("isIntersected");
+//         }
+//     })
+// }
+
+// const observer = new IntersectionObserver(showImage, {
+//     root: null,
+//     rootMargin: '0px 0px 0px 0px',
+//     threshold: 0.3,
+// });
+
+
+// elements.forEach(element=>{
+//     observer.observe(element);
+// })
 
 
